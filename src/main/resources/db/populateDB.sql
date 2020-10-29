a1e887d8c4735b10eaf93db3e58a1da0e384f52b
Artur Demidov <atanava777@gmail.com>
2020-10-29 17:10:41 +0200

    refactor DB sql files to ManyToMany

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -20,29 +20,41 @@ INSERT INTO restaurants (name)
 VALUES  ('Gloria'),
         ('Troika');
 
-INSERT INTO dishes (restaurant_id, name, is_in_menu, price)
-VALUES  (100003, 'Salad', true, 400),
-        (100003, 'Soup', true, 530),
-        (100003, 'Meat', true, 750),
-        (100003, 'Fish', true, 960),
-        (100003, 'Juice', true, 200),
-        (100004, 'Salad', true, 320),
-        (100004, 'Soup', true, 400),
-        (100004, 'Meat', true, 550),
-        (100004, 'Fish', true, 650),
-        (100004, 'Juice', true, 120);
+INSERT INTO dishes (restaurant_id, name, price, active)
+VALUES  (100003, 'Salad', 400, true),
+        (100003, 'Soup', 530, true),
+        (100003, 'Meat', 750, true),
+        (100003, 'Fish', 960, true),
+        (100003, 'Juice', 200, true),
+        (100004, 'Salad', 320, true),
+        (100004, 'Soup', 400, true),
+        (100004, 'Meat', 550, true),
+        (100004, 'Fish', 650, true),
+        (100004, 'Juice', 120, true);
 
-INSERT INTO menus (restaurant_id, dish_id)
-VALUES  (100003, 100005),
-        (100003, 100006),
-        (100003, 100007),
-        (100003, 100008),
-        (100003, 100009),
-        (100004, 100010),
-        (100004, 100011),
-        (100004, 100012),
-        (100004, 100013),
-        (100004, 100014);
+INSERT INTO menu_items (restaurant_id)
+VALUES (100003),
+        (100003),
+        (100003),
+        (100003),
+        (100003),
+        (100004),
+        (100004),
+        (100004),
+        (100004),
+        (100004);
+
+INSERT INTO menus (dish_id, menu_item_id)
+VALUES  (100005, 100015),
+        (100006, 100016),
+        (100007, 100017),
+        (100008, 100018),
+        (100009, 100019),
+        (100010, 100020),
+        (100011, 100021),
+        (100012, 100022),
+        (100013, 100023),
+        (100014, 100024);
 
 INSERT INTO votes (restaurant_id, user_id)
 VALUES (100003, 100001),
